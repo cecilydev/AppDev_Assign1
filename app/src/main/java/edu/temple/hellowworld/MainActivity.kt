bcb8ef22613c35b92325fa2eb418f5befcc15a60
Cecily Devaney <tul06320@temple.edu>
2021-09-12 14:03:08 -0400

    Added button that when clicked changes textview to reflect button was clicked

--- app/src/main/java/edu/temple/hellowworld/MainActivity.kt
+++ app/src/main/java/edu/temple/hellowworld/MainActivity.kt
@@ -2,10 +2,15 @@ package edu.temple.hellowworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button)
+            .setOnClickListener{findViewById<TextView>(R.id.textView).text = "You clicked the button"}
     }
 }
